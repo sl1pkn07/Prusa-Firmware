93238aa75f8916d9280e68352475c4132c4efb40
sL1pKn07 <sl1pkn07@gmail.com>
2024-07-07 14:56:21 +0200

    MK2.9

--- Firmware/printers.h
+++ Firmware/printers.h
@@ -12,20 +12,27 @@
 #define PRINTER_MK2_SNMM           201            // better is "10200"
 #define PRINTER_MK2_SNMM_NAME      "MK2MM"        // better is "MK2MMU1"
 // *** MK2S ??? is same as "MK2" ???
-#define PRINTER_MK2S	          202
+#define PRINTER_MK2S	           202
 #define PRINTER_MK2S_NAME          "MK2S"
 #define PRINTER_MK2S_SNMM          203            // better is "10202"
 #define PRINTER_MK2S_SNMM_NAME     "MK2SMM"       // better is "MK2SMMU1"
 // *** MK2.5
-#define PRINTER_MK25	          250
+#define PRINTER_MK25	           250
 #define PRINTER_MK25_NAME          "MK2.5"
 #define PRINTER_MK25_MMU2          20250
 #define PRINTER_MK25_MMU2_NAME     "MK2.5MMU2"
 // *** MK2.5S
-#define PRINTER_MK25S	     	252
+#define PRINTER_MK25S	     	   252
 #define PRINTER_MK25S_NAME         "MK2.5S"
 #define PRINTER_MK25S_MMU2         20252
 #define PRINTER_MK25S_MMU2_NAME    "MK2.5SMMU2S"
+// *** MK2.9S
+#define PRINTER_MK29S	     	   252
+#define PRINTER_MK29S_NAME         "MK2.9S"
+#define PRINTER_MK29S_MMU2         20302
+#define PRINTER_MK29S_MMU2_NAME    "MK3SMMU2S"
+#define PRINTER_MK29S_MMU3         30302
+#define PRINTER_MK29S_MMU3_NAME    "MK3SMMU3"
 // *** MK3
 #define PRINTER_MK3                300
 #define PRINTER_MK3_NAME           "MK3"
@@ -34,7 +41,7 @@
 #define PRINTER_MK3_MMU3           30300
 #define PRINTER_MK3_MMU3_NAME      "MK3MMU3"
 // *** MK3S
-#define PRINTER_MK3S     		302
+#define PRINTER_MK3S     		   302
 #define PRINTER_MK3S_NAME          "MK3S"
 #define PRINTER_MK3S_MMU2          20302
 #define PRINTER_MK3S_MMU2_NAME     "MK3SMMU2S"
